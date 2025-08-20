0acd60264b883dbac7d515092a5f68adf0710bb8
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-08-20 19:52:19 -0300

    Criado o componente BuscadorCep

--- src/components/BuscadorCep.jsx
+++ src/components/BuscadorCep.jsx
@@ -1,11 +1,37 @@
 
+// Imports
+
 import styles from "./BuscadorCep.module.css";
+
+// Bibliotecas
+
+import {FiSearch} from 'react-icons/fi';
+
 
 const BuscadorCep = () => {
     return(
         <>
         <div className={styles.container}>
-        <h1>Buscador Cep</h1>
+        <h1 className={styles.title}>Buscador Cep</h1>
+
+        <div className={styles.containerInput}>
+            <input
+             type="text"
+             placeholder="Digite seu cep..."
+              />
+
+              <button className={styles.buttonSearch}>
+                <FiSearch size={25} color="#FFF"/>
+              </button>
+        </div>
+        <main className={styles.main}>
+            <h2>Cep: 79003222</h2>
+
+            <span>Rua teste algum</span>
+            <span>Complemento: algum complemento</span>
+            <span>Vila Rosa</span>
+            <span>Campo Grande - MS</span>
+        </main>
     </div>
         </>
     )
