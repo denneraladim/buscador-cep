eb4420ff2c508bd2e6de7442b1776964d8c7b59e
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-08-21 11:47:33 -0300

    Atualizado

--- src/components/BuscadorCep.jsx
+++ src/components/BuscadorCep.jsx
@@ -1,38 +1,42 @@
+
+
 // Imports
+
 import styles from "./BuscadorCep.module.css";
-import { FiSearch } from "react-icons/fi";
+
+// Bibliotecas
+
+import {FiSearch} from 'react-icons/fi';
+
 
 const BuscadorCep = () => {
-  return (
-    <>
-
-      <section className={styles.container}>
+    return(
+        <>
+        <section className={styles.container}>
         <h1 className={styles.title}>Buscador Cep</h1>
-        <form className={styles.containerInput}>
-          <label htmlFor="cepInput" className="sr-only">
-            Digite seu CEP
-          </label>
-          <input
-            id="cepInput"
-            type="search"
-            placeholder="Digite seu cep..."
-          />
-          <button className={styles.buttonSearch}>
-            <FiSearch size={25} color="#FFF" />
-          </button>
-        </form>
+
+        <div className={styles.containerInput}>
+            <input
+             type="text"
+             placeholder="Digite seu cep..."
+              />
+
+              <button className={styles.buttonSearch}>
+                <FiSearch size={25} color="#FFF"/>
+              </button>
+        </div>
         <main className={styles.main}>
-          <h2>Cep: 79003222</h2>
-          <address>
-            <p>Rua teste algum</p>
-            <p>Complemento: algum complemento</p>
-            <p>Vila Rosa</p>
-            <p>Campo Grande - MS</p>
-          </address>
-          </main>
-      </section>
-    </>
-  );
-};
+            <h2>Cep: 79003222</h2>
+
+            <span>Rua teste algum</span>
+            <span>Complemento: algum complemento</span>
+            <span>Vila Rosa</span>
+            <span>Campo Grande - MS</span>
+        </main>
+    </section>
+        </>
+    )
+    
+}
 
 export default BuscadorCep;
